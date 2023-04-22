deb9f37c853fd6e12aa32a9c7b283fb0fa5f40b7
Le Duc Luong <ducluong3k2@gmail.com>
2023-04-22 22:22:25 +0700

    update gd home, change header

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -10,13 +10,12 @@ const CartController ={
         const carts = await Cart.find({});
         res.render('giohang',{carts: mutipleMongooseToObject(carts)});
     },
-
     addItemGioHang: async(req,res)=>{
         try{
             const {name, image,giaBan,soLuong}= req.body;
             const book = await Cart.findOne({name: name});
             //kiem tra san pham da co trong gio hang
-            if(book) return res.json("msg: Đã tồn tại san pham trong gio");
+            if(book) return res.json({"msg": "Đã tồn tại san pham trong gio"});
             //san pham chua co trong gio hang
             const newCart= new Cart({
                 name, image,giaBan,soLuong
@@ -32,7 +31,10 @@ const CartController ={
         await Cart.deleteOne({_id: req.body.id});
         res.redirect('/cart');
     }
-
+    ,
+    getPay: async(req,res)=>{
+        res.render('menu/thanhToan');
+    }
 }
 
 module.exports = CartController;
--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -5,6 +5,7 @@ const CartController = require('../app/controllers/CartController');
 
 cartRouter.delete('/deletecart',CartController.deleteItem);
 cartRouter.post('/addcart',CartController.addItemGioHang); 
+cartRouter.get('/pay',CartController.getPay);
 cartRouter.get('/',CartController.getGioHang);
 
 module.exports = cartRouter;
