97fc90beef0c6f83f9d4780ec7e92f192770666b
Le Duc Luong <ducluong3k2@gmail.com>
2023-05-15 21:32:54 +0700

    update mongoatlas

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
+// async function connect() {
+//   try {
+//     await mongoose.connect("mongodb://127.0.0.1:27017/f8Education");
+//     console.log("Connected to mongodb");
+//   } catch (error) {
+//     console.log("failed to connect to mongodb");
+//   }
+// }
 async function connect() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/f8Education");
+    await mongoose.connect("mongodb+srv://f8Education:Luong321@cluster0.y7gq4w5.mongodb.net/e-commerce");
     console.log("Connected to mongodb");
   } catch (error) {
     console.log("failed to connect to mongodb");
